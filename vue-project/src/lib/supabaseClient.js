93e87cf0fbdf6744ef0bed1486d8d40e46f6b2f2
Judge09 <133473201+Judge09@users.noreply.github.com>
2025-02-04 07:30:34 +0700

    MIC TEST

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://ljatdwddotduzmamsxzv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqYXRkd2Rkb3RkdXptYW1zeHp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwMjY4OTksImV4cCI6MjA1MzYwMjg5OX0.BiB_o73NpIkh99DEgMUT2N2kIZDnxy1VaWP0KahW-_Q')
+export const supabase = createClient('https://chcdlldnbwxdvjtamzlt.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNoY2RsbGRuYnd4ZHZqdGFtemx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2Mjg4ODcsImV4cCI6MjA1NDIwNDg4N30.PMPlJzcct5eUrMxl-_2r-CbyWbnt4p4sBYxZIF8otfY')
